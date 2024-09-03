71dd42844d0bdb649f207a1a9ac11b93af311e9c
Buddy <deckerd70@yahoo.com>
2024-09-02 21:15:06 -0400

    CPS353-CheckstyleTesting: Added a file that'll fail both checks

---
+++ src/CheckstyleTest.java
@@ -0,0 +1,9 @@
+public class CheckstyleTest {
+    public boolean checkstyleTesting() {
+        boolean boolTest = true;
+
+        if(boolTest == true) {
+            return boolTest;
+        } else return boolTest =? false;
+    }
+}
