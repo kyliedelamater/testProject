f1bc135fa68f689be0a55b72687f6f6ad96c5437
Buddy <deckerd70@yahoo.com>
2024-09-02 21:30:33 -0400

    CPS353-CheckstyleTesting: Adjusted new file to pass checks

--- src/CheckstyleTest.java
+++ src/CheckstyleTest.java
@@ -2,8 +2,6 @@ public class CheckstyleTest {
     public boolean checkstyleTesting() {
         boolean boolTest = true;
 
-        if(boolTest == true) {
-            return boolTest;
-        } else return boolTest =? false;
+        return boolTest;
     }
 }
