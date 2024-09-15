506f01e9b95d2e92b7d3374a50e97f8bcbf9ad0c
Fv40 <deckerd70@yahoo.com>
2024-09-14 20:40:07 -0400

    ComputeApiPrototype: Updated compute api and interface usage"

--- src/api/PrototypeComputeApi.java
+++ src/api/PrototypeComputeApi.java
@@ -1,10 +1,19 @@
 package api;
 
-import interfaces.ComputeRequest;
+import interfaces.ComputeEngine;
+import interfaces.ComputeRequestHandler;
+import interfaces.NumStream;
 
 public class PrototypeComputeApi {
 
-  public void computeRequest(ComputeRequest computeRequest) {
+  public void prototypeComputeRequest(ComputeRequestHandler computeRequestHandler, ComputeEngine computeEngine) {
+    // Get the ints to calculate factorials for, from user request
+    NumStream computeEngineInput = computeRequestHandler.getIntegersForFactorial();
 
+    // Send the ints over to the engine for computation
+    NumStream computeResult = computeEngine.doFactorial(computeEngineInput);
+
+    // Generate a response string and pass it along
+    computeRequestHandler.generateAndSendResponseMessage(computeResult);
   }
 }
--- src/interfaces/ComputeEngine.java
+++ src/interfaces/ComputeEngine.java
@@ -1,5 +1,5 @@
 package interfaces;
 
 public interface ComputeEngine {
-
+  NumStream doFactorial(NumStream numStream);
 }
