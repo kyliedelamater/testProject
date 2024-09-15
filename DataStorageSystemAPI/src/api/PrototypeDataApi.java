156428d1d1a9ee3cf17b800a983bfdb89f7108fd
kyliedelamater <kyliedelamater@gmail.com>
2024-09-15 16:17:52 -0400

    Add files via upload

---
+++ DataStorageSystemAPI/src/api/PrototypeDataApi.java
@@ -0,0 +1,21 @@
+package api;
+
+public class PrototypeDataApi {
+		
+	public void dataSystemPrototype(DataStorageSystem data) {
+		
+		//read from user-specified input
+		ReadInputResponse readInputResponse = new ReadInputRequest();
+		
+		//write to user-specified input
+		WriteInputResponse writeInputResponse = new WriteInputRequest();
+		
+		//read from user-specified output
+		ReadOutputResponse readOutResponse = new ReadOutputRequest();
+		
+		//write to user-specified output
+		WriteOutputResponse writeOutputResponse = new WriteOutputRequest();
+		
+	}
+
+}
