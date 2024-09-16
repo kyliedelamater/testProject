9d3c66e339f29cfc4295869dec8fef62c25065a6
kyliedelamater <kyliedelamater@gmail.com>
2024-09-15 22:36:41 -0400

    removed readOutput and writeInput

--- src/api/DataStorageSystem.java
+++ src/api/DataStorageSystem.java
@@ -4,9 +4,5 @@ public interface DataStorageSystem {
 
 	ReadInputResponse readInput(ReadInputRequest readInputRequest);
 	
-	WriteInputResponse writeInput(WriteInputRequest writeInputRequest);
-	
-	ReadOutputResponse readOutput(ReadOutputRequest readOutputRequest);
-	
 	WriteOutputResponse writeOutput(WriteOutputRequest writeOutputRequest);
 }
