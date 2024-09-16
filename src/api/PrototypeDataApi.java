e8ac60178298ee85cc1fb6e6e2a1547001a2c292
kyliedelamater <kyliedelamater@gmail.com>
2024-09-15 22:53:29 -0400

    Update PrototypeDataApi.java

--- src/api/PrototypeDataApi.java
+++ src/api/PrototypeDataApi.java
@@ -5,16 +5,10 @@ public class PrototypeDataApi {
 	public void dataSystemPrototype(DataStorageSystem data) {
 		
 		//read from user-specified input
-		ReadInputResponse readInputResponse = new ReadInputRequest();
-		
-		//write to user-specified input
-		WriteInputResponse writeInputResponse = new WriteInputRequest();
-		
-		//read from user-specified output
-		ReadOutputResponse readOutResponse = new ReadOutputRequest();
+		ReadInputResponse readInputResponse = data.readInput(new ReadInputRequest());
 		
 		//write to user-specified output
-		WriteOutputResponse writeOutputResponse = new WriteOutputRequest();
+		WriteOutputResponse writeOutputResponse = data.writeOutput(new WriteOutputRequest());
 		
 	}
 
