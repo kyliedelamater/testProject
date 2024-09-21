b3b3dccdec1adf80a841320e3cd769356344ba99
kyliedelamater <kyliedelamater@gmail.com>
2024-09-21 15:16:27 -0400

    data store in memory implementation

---
+++ test/DataStoreInMemoryImplementation.java
@@ -0,0 +1,24 @@
+package datastoreapi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class DataStoreInMemoryImplementation {
+	private Input input;
+	private Output output;
+		
+		@Test
+		public void readAndWriteData() {
+			List<Integer> input2 = input.readInput();
+
+			for(int i = 0; i < input2.size(); i++) {
+				String outputData = "Output = " + input2.get(i);
+				output.writeOutput(outputData);
+			}
+			return;
+		}
+
+	}
+}
