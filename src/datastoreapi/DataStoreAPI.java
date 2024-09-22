437796d88c9e4e02d8f5e60c86f44c0ae8333554
kyliedelamater <kyliedelamater@gmail.com>
2024-09-22 16:18:09 -0400

    Empty implementation of DataStorageAPI (#16)
    * Empty implementation of DataStorageAPI
    
    I'm not sure if I expanded the API too much that I implemented some parts, but nothing actually works, so maybe it's okay.
    
    * Update DataStoreAPI.java
    
    * Update DataStoreAPI.java

---
+++ src/datastoreapi/DataStoreAPI.java
@@ -0,0 +1,41 @@
+package datastoreapi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataStoreAPI {
+	
+	//list where the inputs will be stored
+	private List<Integer> inputList = new ArrayList<>();
+	
+	//instance variable 
+	ComputeEngineAPI computeEngineAPI;
+
+	public DataStoreAPI() {
+		return;
+	}
+	
+	//getter
+	public List<Integer> getInputList() {
+		return inputList;
+	}
+
+	//setter
+	public void setInputList(List<Integer> inputList) {
+		this.inputList = inputList;
+	}
+	
+	public List<Integer> addInput(int a){
+		return inputList;
+	}
+	
+	//input response method
+	public InputResponse readInput(InputRequest inputRequest) {
+		return null;
+	}
+	
+	//output response method
+	public OutputResponse writeOutput(OutputRequest outputRequest) {
+		return null;
+	}
+}
