6c7cb37e2536c813385bf5b9a34da3cb1aed9e9b
EDOLK <119468544+EDOLK@users.noreply.github.com>
2024-09-23 09:15:32 -0400

    implementation and smoke test (#21)

---
+++ src/ComputeEngineApi.java
@@ -0,0 +1,3 @@
+public interface ComputeEngineApi {
+	public EngineResponse submitRequest(UserRequest request);
+}
---
+++ src/UserIOApi.java
@@ -0,0 +1,21 @@
+public class UserIOApi {
+
+	private ComputeEngineApi computeEngineApi;
+
+	public UserIOApi(ComputeEngineApi computeEngineApi) {
+		this.computeEngineApi = computeEngineApi;
+	}
+
+	public ComputeEngineApi getComputeEngineApi() {
+		return computeEngineApi;
+	}
+
+	public void setComputeEngineApi(ComputeEngineApi computeEngineApi) {
+		this.computeEngineApi = computeEngineApi;
+	}
+	
+	public EngineResponse sendRequest(UserRequest request) {
+		return null;
+	}
+
+}
