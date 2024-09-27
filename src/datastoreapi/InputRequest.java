514a0af1f4974778ebc68eef95909ac86510f98b
kyliedelamater <kyliedelamater@gmail.com>
2024-09-27 13:08:25 -0400

    Update InputRequest.java

--- src/datastoreapi/InputRequest.java
+++ src/datastoreapi/InputRequest.java
@@ -1,5 +1,21 @@
 package datastoreapi;
 
 public class InputRequest {
+	private String file;
 
+	public InputRequest(String file) {
+		this.setFile(file);
+	}
+
+	public InputRequest() {
+		return;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
 }
