2948408767d695ec81336a70c36c7ef74cc9fbf2
kyliedelamater <kyliedelamater@gmail.com>
2024-09-29 19:16:08 -0400

    Update MediatorClassTest.java

--- test/MediatorClassTest.java
+++ test/MediatorClassTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.assertEquals;
 
+import interfaces.NumStream;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -7,17 +10,27 @@ import org.junit.jupiter.api.Test;
 public class MediatorClassTest {
 
 	@Test
-	   public void testToStringList() {
+	public void testToStringList() {
 
-        NumStreamImplementation numStream = new NumStreamImplementation();
-        
-        List<Integer> testList = List.of(0, 1, 2, 3);
-        numStream.setIntegerList(testList);  
+       		NumStreamImplementation numStream = new NumStreamImplementation();
 
-        List<String> output = toStringList(numStream);
+     		List<Integer> testList = List.of(0, 1, 2, 3);
+      		numStream.setIntegerList(testList);  
 
-        List<String> expectedOutput = List.of("0", "1" , "2", "3");
+        	List<String> output = toStringList(numStream);
 
-        assertEquals(expectedOutput, output);
+        	List<String> expectedOutput = List.of("0", "1" , "2", "3");
+
+        	assertEquals(expectedOutput, output);
     }
+
+	private List<String> toStringList(NumStream stream){
+		ArrayList<Integer> integers = (ArrayList<Integer>) stream.getIntegers();
+		ArrayList<String> strings = new ArrayList<String>();
+		for (Integer integer : integers) {
+			strings.add(String.valueOf(integer));
+		}
+		return strings;
+
+	}
 }
