fa5b0eef3654a455017a852ac3ec91627dde6b01
Fv40 <Deckerd70@yahoo.com>
2024-09-29 12:32:58 -0400

    Updated datastore test

--- test/DataStoreInMemoryImplementation.java
+++ test/DataStoreInMemoryImplementation.java
@@ -1,17 +1,14 @@
-package datastoreapi;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 public class DataStoreInMemoryImplementation {
-	private InputRequest input;
-	private OutputRequest output;
+	private datastoreapi.InputInMemoryImplementation input;
+	private datastoreapi.OutputInMemoryImplementation output;
 		
 		@Test
 		public void readAndWriteData() {
-			List<Integer> input2 = input.readInput();
+			List<Integer> input2 = input.getInput();
 
 			for(int i = 0; i < input2.size(); i++) {
 				String outputData = "Output = " + input2.get(i);
