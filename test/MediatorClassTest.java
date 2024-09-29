bba96b3b5f1c9ef96505e19a5a07480003bf9ecb
kyliedelamater <kyliedelamater@gmail.com>
2024-09-29 19:42:33 -0400

    Update MediatorClassTest.java

--- test/MediatorClassTest.java
+++ test/MediatorClassTest.java
@@ -27,8 +27,8 @@ public class MediatorClassTest {
     }
 
 	private List<String> toStringList(NumStream stream){
-		List<Integer> integers = (ArrayList<Integer>) stream.getIntegers();
-		List<String> strings = new ArrayList<String>();
+		List<Integer> integers = stream.getIntegers();
+		List<String> strings = new ArrayList<>();
 		for (Integer integer : integers) {
 			strings.add(String.valueOf(integer));
 		}
