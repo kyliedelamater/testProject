d7bfbdaf5ad099213619369a6832a0c1c0fe4ebd
kyliedelamater <kyliedelamater@gmail.com>
2024-09-29 19:32:35 -0400

    Update NumStreamImplementation.java

--- src/NumStreamImplementation.java
+++ src/NumStreamImplementation.java
@@ -1,6 +1,7 @@
 import interfaces.NumStream;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NumStreamImplementation implements NumStream {
   public List<Integer> integerList;
