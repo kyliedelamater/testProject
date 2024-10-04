331bb99932b161721f015a6ee498cc7e03ad5b45
kyliedelamater <kyliedelamater@gmail.com>
2024-10-04 14:19:55 -0400

    Update Mediator.java

--- src/Mediator.java
+++ src/Mediator.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import datastoreapi.DataStoreAPI;
 import datastoreapi.InputRequest;
@@ -7,48 +7,87 @@ import datastoreapi.OutputRequest;
 import interfaces.NumStream;
 
 public class Mediator {
-	
+
 	private static Mediator mediator = new Mediator(new DataStoreAPI(), new ComputeEngineImplementation(new ComputationImplementation(), new ComputeRequestHandlerImplementation()));
-	
+
 	private DataStoreAPI dataStoreApi;
-	
+
 	private ComputeEngine computeEngine;
-	
+
 	public Mediator(DataStoreAPI dataStoreApi, ComputeEngine computeEngine) {
+		if(dataStoreApi == null)) {
+			throw new IllegalArgumentException("DataStoreAPI cannot be null");
+		}
+		if(computeEngine == null) {
+			throw new IllegalArgumentException("ComputeEngine cannot be null");
+		}
 		this.dataStoreApi = dataStoreApi;
 		this.computeEngine = computeEngine;
 	}
-	
+
 	public static Mediator getMediator() {
 		return mediator;
 	}
-	
+
 	public static void setMediator(Mediator replacementMediator) {
+		if(replacementMediator == null) {
+			throw new IllegalArgumentException("Mediator cannot be null");
+		}
 		mediator = replacementMediator;
 	}
-	
+
 	public <T> void sendInput(UserRequestProvider<T> provider) {
+		if(provider == null) {
+			throw new IllegalArgumentException("UserRequestProvider cannot be null");
+		}
 		UserRequest userRequest = provider.generateRequest(provider.getInput());
-		UserRequestDestination destination = userRequest.getUserRequestDestination();
-		UserRequestSource source = userRequest.getUserRequestSource();
+
 		EngineResponse response = new ConcreteEngineResponse(ResponseCode.FAILED);
-		if (source instanceof FileUserRequestSource fileSource && destination instanceof FileUserRequestDestination fileDestination) {
-			InputRequest inputRequest = new InputRequest(fileSource.getFileName());
-			List<Integer> inputList = dataStoreApi.readInput(inputRequest);
-			NumStream inputNumStream = new NumStreamImplementation();
+
+		Optional<InputRequest> inRequest = generateInputRequest(userRequest);
+
+		NumStream inputNumStream = new NumStreamImplementation();
+
+		if (inRequest.isPresent()) {
+			List<Integer> inputList = dataStoreApi.readInput(inRequest.get());
 			inputNumStream.setIntegerList(inputList);
-			EngineResponse engineResponse = computeEngine.submitRequest(userRequest);
-			dataStoreApi.setOutputList(List.of(engineResponse.getRequestResult().getResultString()));
-			OutputRequest outputRequest = new OutputRequest(fileDestination.getFileName());
-			List<String> outputList = dataStoreApi.writeOutput(outputRequest);
-			if (!outputList.isEmpty()) {
+		}
+
+		userRequest.setRequestStream(inputNumStream);
+
+		EngineResponse engineResponse = computeEngine.submitRequest(userRequest);
+
+		if (!engineResponse.getResponseCode().isFailure()) {
+			Optional<OutputRequest> outRequest = generateOutputRequest(userRequest);
+			if (outRequest.isPresent()) {
+				dataStoreApi.setOutputList(List.of(engineResponse.getRequestResult().getResultString()));
+				dataStoreApi.writeOutput(outRequest.get());
 				response.setResponseCode(ResponseCode.SUCCESSFUL); 
 				response.setRequestResult(engineResponse.getRequestResult());
 			}
-		} else {
-			response = new ConcreteEngineResponse(ResponseCode.UNIMPLEMENTED);
 		}
+
 		provider.propigateResponse(response);
 	}
-	
+
+	private Optional<InputRequest> generateInputRequest(UserRequest request) {
+		if(request == null) {
+			throw new IllegalArgumentException("UserRequest cannot be null");
+		}
+		if (request.getUserRequestSource() instanceof FileUserRequestSource fileSource) {
+			return Optional.of(new InputRequest(fileSource.getFileName()));
+		}
+		return Optional.empty();
+	}
+
+	private Optional<OutputRequest> generateOutputRequest(UserRequest request) {
+		if(request == null) {
+			throw new IllegalArgumentException("UserRequest cannot be null");
+		}
+		if (request.getUserRequestDestination() instanceof FileUserRequestDestination fileDestination) {
+			return Optional.of(new OutputRequest(fileDestination.getFileName()));
+		}
+		return Optional.empty();
+	}
+
 }
