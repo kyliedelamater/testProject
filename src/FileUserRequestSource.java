90744baaa3ad65bb5d6aa0adc30b70011089fd0d
kyliedelamater <kyliedelamater@gmail.com>
2024-10-04 14:11:08 -0400

    Update FileUserRequestSource.java

--- src/FileUserRequestSource.java
+++ src/FileUserRequestSource.java
@@ -4,6 +4,9 @@ public class FileUserRequestSource extends UserRequestSource {
 	private String fileName;
 
 	public FileUserRequestSource(String fileName) {
+		if(fileName == null || fileName.isEmpty()) {
+			throw new IllegalArgumentException("File name cannot be null or empty");
+		}
 		this.fileName = fileName;
 	}
 
