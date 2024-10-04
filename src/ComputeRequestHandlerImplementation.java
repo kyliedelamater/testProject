97f15c8015586f6a245cd562225a0149fd50434d
kyliedelamater <kyliedelamater@gmail.com>
2024-10-04 14:03:30 -0400

    Update ComputeRequestHandlerImplementation.java

--- src/ComputeRequestHandlerImplementation.java
+++ src/ComputeRequestHandlerImplementation.java
@@ -5,41 +5,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComputeRequestHandlerImplementation implements ComputeRequestHandler {
-  DataStoreAPI dataStoreAPI;
-  UserRequest userRequest;
+	DataStoreAPI dataStoreAPI;
+	UserRequest userRequest;
 
-  public ComputeRequestHandlerImplementation() {
-  }
+	public ComputeRequestHandlerImplementation() {
+	}
 
-  public ComputeRequestHandlerImplementation(UserRequest userRequest) {
-    this.userRequest = userRequest;
-  }
+	public ComputeRequestHandlerImplementation(UserRequest userRequest) {
+		if(userRequest == null) {
+			throw new IllegalArgumentException("UserRequest cannot be null");
+		}
+		this.userRequest = userRequest;
+	}
 
-  public ComputeRequestHandlerImplementation(DataStoreAPI dataStoreAPI, UserRequest userRequest) {
-    this.dataStoreAPI = dataStoreAPI;
-    this.userRequest = userRequest;
-  }
+	public ComputeRequestHandlerImplementation(DataStoreAPI dataStoreAPI, UserRequest userRequest) {
+		if(userRequest == null) {
+			throw new IllegalArgumentException("UserRequest cannot be null");
+		}
+		if(dataStoreAPI == null) {
+			throw new IllegalArgumentException("DataStoreAPI cannot be null");
+		}
+		this.dataStoreAPI = dataStoreAPI;
+		this.userRequest = userRequest;
+	}
 
-  @Override
-  public void setUserRequest(UserRequest userRequest) {
-    this.userRequest = userRequest;
-  }
+	@Override
+	public void setUserRequest(UserRequest userRequest) {
+		if(userRequest == null) {
+			throw new IllegalArgumentException("UserRequest cannot be null");
+		}
+		this.userRequest = userRequest;
+	}
 
-  @Override
-  public UserRequest getUserRequest() {
-    return userRequest;
-  }
+	@Override
+	public UserRequest getUserRequest() {
+		return userRequest;
+	}
 
 
-  @Override
-  public ResponseCode generateAndSendResponseMessage(NumStream computeResults) {
-    return null;
-  }
+	@Override
+	public ResponseCode generateAndSendResponseMessage(NumStream computeResults) {
+		return null;
+	}
 
-  @Override
-  public <T> void setDataApi(T dataStorageApi) {
-    this.dataStoreAPI = (DataStoreAPI) dataStorageApi;
-  }
+	@Override
+	public <T> void setDataApi(T dataStorageApi) {
+		if(dataStoreAPI == null) {
+			throw new IllegalArgumentException("DataStoreAPI cannot be null");
+		}
+		this.dataStoreAPI = (DataStoreAPI) dataStorageApi;
+	}
 }
 
 
