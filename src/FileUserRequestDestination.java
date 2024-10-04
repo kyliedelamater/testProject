1a855c06af54599d88e45189a1155443c4b9e84f
kyliedelamater <kyliedelamater@gmail.com>
2024-10-04 14:10:41 -0400

    Update FileUserRequestDestination.java

--- src/FileUserRequestDestination.java
+++ src/FileUserRequestDestination.java
@@ -4,6 +4,9 @@ public class FileUserRequestDestination extends UserRequestDestination {
 	private String fileName;
 
 	public FileUserRequestDestination(String fileName) {
+		if(fileName == null || fileName.isEmpty()) {
+			throw new IllegalArgumentException("File name cannot be null or empty");
+		}
 		this.fileName = fileName;
 	}
 
