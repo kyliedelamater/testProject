9ca5b367b3996d319709cd505e873df24a347a13
kyliedelamater <kyliedelamater@gmail.com>
2024-10-04 14:08:50 -0400

    Update EngineResponseImplementation.java

--- src/EngineResponseImplementation.java
+++ src/EngineResponseImplementation.java
@@ -2,34 +2,46 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class EngineResponseImplementation implements EngineResponse {
-  ResponseCode responseCode;
-  RequestResult requestResult;
+	ResponseCode responseCode;
+	RequestResult requestResult;
 
-  public EngineResponseImplementation() {
-  }
+	public EngineResponseImplementation() {
+	}
 
-  public EngineResponseImplementation(ResponseCode responseCode, ArrayList<Integer> requestResult) {
-    this.responseCode = responseCode;
-    this.requestResult = new RequestResultImplementation(requestResult);
-  }
+	public EngineResponseImplementation(ResponseCode responseCode, ArrayList<Integer> requestResult) {
+		if(responseCode == null) {
+			throw new IllegalArgumentException("ResponseCode cannot be null");
+		}
+		if(requestResult == null) {
+			throw new IllegalArgumentException("Result list cannot be null");
+		}
+		this.responseCode = responseCode;
+		this.requestResult = new RequestResultImplementation(requestResult);
+	}
 
-  @Override
-  public void setRequestResult(RequestResult result) {
-    this.requestResult = result;
-  }
+	@Override
+	public void setRequestResult(RequestResult result) {
+		if(result == null) {
+			throw new IllegalArgumentException("RequestResult cannot be null");
+		}
+		this.requestResult = result;
+	}
 
-  @Override
-  public void setResponseCode(ResponseCode responseCode) {
-    this.responseCode = responseCode;
-  }
+	@Override
+	public void setResponseCode(ResponseCode responseCode) {
+		if(responseCode == null) {
+			throw new IllegalArgumentException("ResponseCode cannot be null");
+		}
+		this.responseCode = responseCode;
+	}
 
-  @Override
-  public ResponseCode getResponseCode() {
-    return this.responseCode;
-  }
+	@Override
+	public ResponseCode getResponseCode() {
+		return this.responseCode;
+	}
 
-  @Override
-  public RequestResult getRequestResult() {
-    return this.requestResult;
-  }
+	@Override
+	public RequestResult getRequestResult() {
+		return this.requestResult;
+	}
 }
