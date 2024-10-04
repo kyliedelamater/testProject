1e5ca5b761a15a09a761da09b537ef47b467a850
kyliedelamater <kyliedelamater@gmail.com>
2024-10-04 15:17:12 -0400

    Update InputRequest.java

--- src/datastoreapi/InputRequest.java
+++ src/datastoreapi/InputRequest.java
@@ -4,6 +4,9 @@ public class InputRequest {
 	private String file;
 
 	public InputRequest(String file) {
+	if(file == null || file.isEmpty()) {
+		throw new IllegalArgumentException("Input file cannot be null or empty");
+	}
 		this.setFile(file);
 	}
 
@@ -16,6 +19,9 @@ public class InputRequest {
 	}
 
 	public void setFile(String file) {
+	if(file == null || file.isEmpty()) {
+		throw new IllegalArgumentException("Input file cannot be null or empty");
+	}
 		this.file = file;
 	}
 
