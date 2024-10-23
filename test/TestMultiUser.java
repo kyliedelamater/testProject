d44282d9ab582af8c8cd612e0d1545a008574dcc
kyliedelamater <kyliedelamater@gmail.com>
2024-10-23 16:13:20 -0400

    Update TestMultiUser.java

--- test/TestMultiUser.java
+++ test/TestMultiUser.java
@@ -17,13 +17,15 @@ public class TestMultiUser {
 	
 	// TODO 1: change the type of this variable to the name you're using for your
 	// User <-> ComputeEngine API
-	private ComputationCoordinator coordinator;
+	private UserIOApi coordinator;
 	
 	@BeforeEach
 	public void initializeComputeEngine() {
 		//TODO 2: create an instance of your coordinator component; this is the component
 		// that the user will make requests to
 		// Store it in the 'coordinator' instance variable
+		coordinator = new UserIOApi(new ComputeEngineApi());
+
 	}
 
 	@Test
