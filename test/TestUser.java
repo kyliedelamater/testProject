bb9f7ee8dd79176599316db14a101337cf679902
kyliedelamater <kyliedelamater@gmail.com>
2024-10-24 20:13:15 -0400

    did TODOs

--- test/TestUser.java
+++ test/TestUser.java
@@ -7,9 +7,9 @@ public class TestUser {
 	
 	// TODO 3: change the type of this variable to the name you're using for your
 	// User <-> ComputeEngine API; also update the parameter passed to the constructor
-	private final ComputationCoordinator coordinator;
+	private final UserIOApi coordinator;
 
-	public TestUser(ComputationCoordinator coordinator) {
+	public TestUser(UserIOApi coordinator) {
 		this.coordinator = coordinator;
 	}
 
@@ -19,6 +19,10 @@ public class TestUser {
 		
 		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
 		// run the compute job specified by inputPath, outputPath, and delimiter
+		UserRequestSource userRequestSource = new UserRequestSource(inputPath);
+		UserRequestDestination userRequestDestination = new UserRequestDestinatin(outputPath);
+		UserRequest userRequest = new UserRequest(userRequestSource, userRequestDestination, delimiter);
+		coordinator.sendRequest(userRequest);
 	}
 
 }
