f2ad2f5334bdc0ccd843025695dec179c57ac87d
kyliedelamater <kyliedelamater@gmail.com>
2024-10-24 20:00:14 -0400

    added String inputPath

--- src/UserRequestSource.java
+++ src/UserRequestSource.java
@@ -1,7 +1,20 @@
 
 public class UserRequestSource {
-  
-        public UserRequestSource(){
+    String inputPath;
 
+    public UserRequestSource(String inputPath){
+        this.inputPath = inputPath;   
     }
+
+    public UserRequestSource(){
+    }
+    
+    public String getInputPath() {
+        return inputPath;
+    }
+    
+    public void setInputPath(String inputPath) {
+        this.inputPath = inputPath;
+    }   
+
 }
