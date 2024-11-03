12d61aca14d3b4f5af99bb3cd67013f5d4019614
Fv40 <deckerd70@yahoo.com>
2024-11-03 00:50:27 -0400

    gRPC-Server: Added server startup to main method

--- src/CommandLineEntryPoint.java
+++ src/CommandLineEntryPoint.java
@@ -7,7 +7,11 @@ public class CommandLineEntryPoint implements UserRequestProvider<String[]>{
 		this.args = args;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		ComputeServer computeServer = new ComputeServer();
+		computeServer.start();
+		computeServer.blockUntilShutdown();
+
 		if (args[0].equals("--help")) {
 			System.out.println("Command line entry point for factorial digit sum program. Takes filename for input and filename to output.");
 			System.out.println("Ex: CommandLineEntryPoint inputfilename.txt outputfile.txt");
---
+++ src/ComputeServer.java
@@ -0,0 +1,43 @@
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
+import io.grpc.protobuf.services.ProtoReflectionService;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class ComputeServer {
+  private Server server;
+
+  void start() throws IOException {
+    int port = 8010;
+
+    server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
+            //.addService(new -)
+            .addService(ProtoReflectionService.newInstance())
+            .build()
+            .start();
+    System.out.println("Server started on port " + port);
+
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        System.err.println("closing server");
+        try {
+          if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+          }
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
+        }
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  public void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+}
